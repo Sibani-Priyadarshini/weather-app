1a6bb769d957a571989b49169b58dbec71d5befb
Sibani-Priyadarshini <sibanipriyadarshini130@gmail.com>
2025-05-05 15:41:01 +0530

    Initial commit

---
+++ src/utilities/DatetimeUtils.js
@@ -0,0 +1,47 @@
+import { MONTHS, DAYS } from './DateConstants';
+
+export function getWeekDays() {
+  const dayInAWeek = new Date().getDay();
+  const days = DAYS.slice(dayInAWeek, DAYS.length).concat(
+    DAYS.slice(0, dayInAWeek)
+  );
+  return days;
+}
+
+export function getDayMonthFromDate() {
+  const date = new Date();
+  const month = MONTHS[date.getMonth()].slice(0, 3);
+  const day = date.getUTCDate();
+
+  return day + ' ' + month;
+}
+
+export function transformDateFormat() {
+  const date = new Date();
+  const month = date.toLocaleString('en-US', { month: '2-digit' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+  const time = date.toLocaleString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hourCycle: 'h23',
+  });
+
+  const newFormatDate = `${year}-${month}-${day} ${time}`;
+  return newFormatDate;
+}
+
+export function getISTDatetime() {
+  const date = new Date(); // fresh date object
+  return date.toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    // second: '2-digit',
+    hour12: true,
+  });
+}
